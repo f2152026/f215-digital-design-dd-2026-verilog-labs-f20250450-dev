95e056eb072898402bc2c84d2852281599b53ce3
f20250450-dev <f20250450@pilani.bits-pilani.ac.in>
2026-09-19 16:52:12 +0000

    lab02 task2

--- labs/lab02/task2/tb.v
+++ labs/lab02/task2/tb.v
@@ -4,8 +4,20 @@
 module tb;
 
   // TODO: declare the inputs and outputs
+  // Inputs
+  reg [1:0] t_sel;
+
+  // Output
+  wire [7:0] t_dout;
+
+
 
   // TODO: instantiate DUT here
+  lut DUT
+(
+    .sel(t_sel),
+    .dout(t_dout)
+);
 
   // Waveform dump configuration (DO NOT CHANGE)
   string vcd_file;
@@ -17,11 +29,28 @@ module tb;
   end
 
   initial begin
-    // TODO: apply different input combinations
 
-  end
+    t_sel = 0;
+    #5;
+
+    t_sel = 1;
+    #5;
+
+    t_sel = 2;
+    #5;
+
+    t_sel = 3;
+    #5;
+
+
+    $finish;
+
+end
 
   initial
-    $monitor($time, " I0=%b I1=%b S=%b | Y=%b", t_i0, t_i1, t_s, t_y); // change as required
+    $monitor($time,
+" sel=%b | dout=%d",
+t_sel,
+t_dout);
 
 endmodule
